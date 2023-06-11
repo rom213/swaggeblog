a2fe5b79dd8e59de9f6d51e8dafac441dfb61550
Rom213 <romarioariza@outlook.es>
2023-06-11 12:12:26 -0500

    docs

--- app.js
+++ app.js
@@ -12,17 +12,25 @@ const options = {
     info: {
         title: 'Blog API',
         version: '1.0.0',
+        description: 'Esta es la documentación de la API de Mi Aplicación. Aquí encontrarás información detallada sobre los endpoints y cómo interactuar con ellos.',
+            
+        contact: {
+            name: 'Romario Ariza',
+            email: 'romarioariza@gmail.com'
+        }
     },
     servers: [
         {
-            url: 'https://3.12.162.138', // Cambia la URL base aquí
+            url: 'https://3.12.162.138:80', // Cambia la URL base aquí
         },
     ],
 },
 apis: ['./routes/suaggerRoutes.js']
 };
 
+
 const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
 
 // Configurar los encabezados CORS
@@ -32,11 +40,14 @@ app.use(cors());
 app.use('/render',swaggerRoute)
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+
+
 // Ruta para obtener el archivo JSON de la documentación Swagger
 app.get('/api/v1/docs.json', (req, res) => {
   res.setHeader('Content-type', 'application/json');
   res.send(swaggerSpec);
 });
+
 
 module.exports = app; // Exportar la aplicación Express
 
